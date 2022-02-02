81c4859652ad8569d4c9306c506603cf48996de5
Andrei Banu <andreibanu97@yahoo.com>
2022-02-02 11:38:24 +0200

    routing parameters

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -10,6 +10,7 @@ import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
+    {path: 'dishdetail/:id', component: DishdetailComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'contactus', component: ContactComponent}
 ];
